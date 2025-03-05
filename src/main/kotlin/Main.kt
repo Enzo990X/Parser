75675af93d9272ab53629d66386118dd47b0239f
Enzo990X <bkanurkin@live.com>
2025-03-05 22:59:00 +0300

    KS-5-8 task created

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
 fun main() {
-    println("Hello world!")
+
+    val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
+    val doc: Document = Jsoup.connect(url).get()
+
+    println(doc.title())
+
+    for (element in doc.select(".sc-2aegk7-2")) {
+        println(element.text())
+    }
 }
